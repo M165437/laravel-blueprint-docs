735fd51c19409359b1804ea40a16d98b9ec31142
Michael Schmidt-Voigt <michael@schmidt-voigt.de>
2018-03-14 13:52:01 +1300

    Update sticky sidebar and other front-end dependencies

--- resources/assets/js/blueprintdocs.js
+++ resources/assets/js/blueprintdocs.js
@@ -1,3 +1,4 @@
+window.ResizeSensor = require('css-element-queries/src/ResizeSensor');
 require('sticky-sidebar/dist/sticky-sidebar.js');
 
 var hljs = require('highlight.js/lib/highlight');
@@ -9,40 +10,46 @@ $(document).ready(function () {
         topSpacing: 22,
         bottomSpacing: 0,
         containerSelector: '.content-wrapper',
-        innerWrapperSelector: '.sidebar__inner'
+        innerWrapperSelector: '.sidebar__inner',
+        resizeSensor: true
     });
 
     $(window).resize(function() {
-        console.log('Resizing', sidebar);
         sidebar.updateSticky();
     });
 
     $('[id^=collapse-]').on('show.bs.collapse', function () {
         var id = $(this).prev().find('a').data('group-id'),
             scrollTop = $(document.getElementById(id)).offset().top - 33;
+
         $('html, body').animate({scrollTop: scrollTop});
     });
 
     $('[id^=collapse-]').on('hide.bs.collapse', function (e) {
         var id = $(this).prev().find('a').data('group-id'),
             scrollTop = $(document.getElementById(id)).offset().top - 33;
-        if ($(window).scrollTop() !== scrollTop) {
-            $('html, body').animate({scrollTop: scrollTop});
+
+        $('html, body').animate({scrollTop: scrollTop});
+
+        if ($(window).scrollTop() < scrollTop - 1 || $(window).scrollTop() > scrollTop + 1) {
             e.preventDefault();
         }
-        ;
     });
 
     $('.tabs').on('click', 'a', function (e) {
         var id = $(this).attr('href').substring(1),
             scrollTop = $(document.getElementById(id)).offset().top - 33;
+
         $('html, body').animate({scrollTop: scrollTop});
+
         e.preventDefault();
     });
 
     $('.nav-pills').on('click', '.active a', function (e) {
-        e.preventDefault();
         var that = $(this);
+
+        e.preventDefault();
+
         window.setTimeout(function () {
             that.closest('.nav-pills').next('.tab-content').find('.tab-pane').removeClass('active');
             that.parent('li').removeClass('active');
